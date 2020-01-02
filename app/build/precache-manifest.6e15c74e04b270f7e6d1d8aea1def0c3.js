f5ed72e9fc74283f51ed4c7621be5010bb4aa200
redhair <thomasbonanni@afito.com>
2020-01-01 20:20:34 -0500

    added build

---
+++ app/build/precache-manifest.6e15c74e04b270f7e6d1d8aea1def0c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7900f718dbf695e80fb0c2c4d4683d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d7c54e55db20a302d1f",
+    "url": "/static/css/main.0c704b0d.chunk.css"
+  },
+  {
+    "revision": "0613f1d71d70261a9ccf",
+    "url": "/static/js/2.179a65d6.chunk.js"
+  },
+  {
+    "revision": "d712548cdf04a4c9cab29915db16a4e1",
+    "url": "/static/js/2.179a65d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d7c54e55db20a302d1f",
+    "url": "/static/js/main.6eb455bb.chunk.js"
+  },
+  {
+    "revision": "4b37c141b134dd60e39a",
+    "url": "/static/js/runtime-main.84ee5a3c.js"
+  }
+]);
