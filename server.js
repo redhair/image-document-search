55b5ce79830f0a1ce748ad9c6c67f55592054b68
redhair <thomasbonanni@afito.com>
2020-01-01 20:10:42 -0500

    changed server file

--- server.js
+++ server.js
@@ -6,14 +6,25 @@ const compression = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
 const fs = require('fs');
-const https = require('https');
+const http = require('http');
 const logger = require('morgan');
 const path = require('path');
-const sslport = process.env.PORT || 5000;
-const routes = require('./routes/index');
+const port = process.env.PORT || 5000;
+const Tesseract = require('tesseract.js');
+const algoliasearch = require('algoliasearch');
+const client = algoliasearch(process.env.ALGOLIA_KEY, process.env.ALGOLIA_SECRET);
+const index = client.initIndex('image-document-search');
+const uuid = require('uuid/v1');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_PUBLISHABLE_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: 'us-east-2',
+  signatureVersion: 'v4'
+});
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(logger('dev'));
 app.use(cors());
 app.use(compression());
@@ -21,23 +32,54 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/', routes);
+app.get('/getS3Url', (req, res, next) => {
+  let key;
+  let bucket = 'image-document-search';
 
-var privateKey = fs.readFileSync('ssl/server-key.pem');
-var certificate = fs.readFileSync('ssl/server-cert.pem');
+  const makeResponse = (type, key) => {
+    s3.getSignedUrl(
+      'putObject',
+      {
+        Bucket: bucket,
+        ContentType: type,
+        Key: key
+      },
+      (err, url) => res.send({ key, url })
+    );
+  }
 
-https
-  .createServer(
-    {
-      key: privateKey,
-      cert: certificate
-    },
-    app
-  )
-  .listen(sslport, function() {
-    console.log(new Date().toISOString() + ': server started securely on ' + sslport);
-  });
+  key = `${uuid()}.jpg`;
+  return makeResponse('jpg', key);
+});
+
+app.post('/images', (req, res, next) => {
+  Tesseract.recognize(
+    req.body.image,
+    'eng',
+    { logger: m => console.log(m) }
+  ).then(({ data: { text } }) => {
+    let image = {
+      url: req.body.image,
+      text: text
+    }
+
+    index.addObjects([image], (err, content) => {
+      if (err) next(err);
+
+      res.json({content})
+    });
+  }).catch(next)
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen(port, function() {
+  console.log(new Date().toISOString() + ': server started securely on ' + port);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
